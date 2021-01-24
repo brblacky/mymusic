88f460df6a1223d34a3c6940d94ae195d679c7c4
S Dip <77108939+brblacky@users.noreply.github.com>
2021-01-24 21:57:15 +0530

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,21 +11,27 @@ module.exports = {
             .map(c => `\`${c.name}\``);
 
         const embed = util.embed()
-            .setAuthor("Command List", msg.client.user.displayAvatarURL())
-            .setDescription(commands.join(", "))
+            .setAuthor("Commands of "+msg.client.user.username, "https://raw.githubusercontent.com/brblacky/mymusic/main/assets/Added.gif?token=ASMJNSYITTD3U5HXWPCFPWDABUY4Q")
+       
+     .setDescription(commands.join(", "))
+   
+    .setImage('https://cdn.discordapp.com/attachments/592376487974535178/802051186299633704/1611294197856.jpg')
+    
+   .setTimestamp()
             
-            .setTimestamp()
-            
-           .addField(
+     
+    
+    .addField(
+    "Invite link: ", `**[Click Here!](https://discord.com/oauth2/authorize?client_id=648733614929870848&permissions=70282305&scope=bot)**`,
+     true
+     )
+     .addField(
       "Support Link: ",
       `**[Click Here!](https://discord.gg/hfqcGVTNZ9)**`,
      true
-    )
-    .addField(
-    "Invite link: ", `**[Click Here!](https://discord.com/oauth2/authorize?client_id=648733614929870848&permissions=70282305&scope=bot)**`, 
-    
-    )
-    
-    msg.channel.send(embed);
+   
+       )
+         msg.channel.send(embed);
     }
-};
+  };
+
